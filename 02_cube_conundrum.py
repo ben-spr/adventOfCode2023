f4c5e66e81688db15770a8d4d6c88d8e42622160
Sprick, Benjamin (EXTERN: umlaut sys) <benjamin.sprick@accenture.com>
2023-12-20 18:00:41 +0100

    Solved task 2.

--- 02_cube_conundrum.py
+++ 02_cube_conundrum.py
@@ -48,24 +48,35 @@ def find_max_dice_numbers(draws: list):
 
     return seen_colors
 
-
-
-def main():
-    input_path = pathlib.Path(__file__).parent.resolve() / REL_PATH_INPUT_FILE
+def task_1(games: list[str]):
     result = 0
-    games = base_functions.read_input_file_lines_to_list(input_path)
     for game in games:
-    #     seen_colors = read_game_info(game)
-    # for game in example.split('\n'):
         game_number, draws = read_game_info(game)
         max_seen_colors = find_max_dice_numbers(draws)
         colors_possible = all([max_seen_colors[color] <= MAX_DICE_NUMBERS[color] for color in COLORS])
         if colors_possible:
             result += game_number
-        # time.sleep(0.1)
     
-    print(result)
-    time.sleep(0.1)
+    print(f"The sum of all games that would have been possible with the dice set {MAX_DICE_NUMBERS} is: {result}")
+
+def task_2(games):
+    result = 0
+    for game in games:
+        _, draws = read_game_info(game)
+        min_dice_numbers = find_max_dice_numbers(draws)
+        power = 1
+        for dice_number in min_dice_numbers.values():
+            power *= dice_number
+        result += power
+
+    print(f"The sum of all power of dice is: {result}")
+
+def main():
+    input_path = pathlib.Path(__file__).parent.resolve() / REL_PATH_INPUT_FILE
+    games = base_functions.read_input_file_lines_to_list(input_path)
+    # task_1(games)
+    task_2(games)
+    
 
 if __name__ == "__main__":
     main()
