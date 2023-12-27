39ab3c85ec8744e918bce9ccdab5d5eee1cca6d1
Benjamin Sprick <benjamin.sprick@gmx.de>
2023-12-27 19:17:18 +0100

    Restructured generic solution class, adapted day 3 implementation accordingly.

--- base_functions.py
+++ base_functions.py
@@ -1,12 +1,30 @@
+import abc
+import json
+import re
 import pathlib
 
-def read_input_file_to_string(path: pathlib.Path) -> str:
-    filepath = path
-    with open(filepath, 'r') as f:
+
+class Solution:
+    def __init__(self, path_to_input_file: str):
+        if not self.validate_input_file_path(path_to_input_file):
+            raise FileNotFoundError
+        self.input_str = read_text_file_to_string(path_to_input_file)
+        
+    @abc.abstractmethod
+    def solve(self):
+        pass
+
+    def validate_input_file_path(self, path_to_input_file: pathlib.Path):
+        regex = re.compile('\\/(?P<filename>.*).txt$')
+        return not re.search(regex, str(path_to_input_file)) is None
+        
+
+def read_text_file_to_string(path: pathlib.Path) -> str:
+    with open(path, 'r') as f:
         file_text = f.read()
     return file_text
 
-def read_input_file_lines_to_list(path: pathlib.Path) -> list:
+def read_text_file_lines_to_list(path: pathlib.Path) -> list:
     filepath = path
     line_list = []
     with open(filepath, 'r') as f:
@@ -14,3 +32,8 @@ def read_input_file_lines_to_list(path: pathlib.Path) -> list:
             line_list.append(line)
 
     return line_list
+
+def read_json_file(path: pathlib.Path) -> dict:
+    with open(path, 'r') as f:
+        file_json = json.load(f)
+    return file_json
